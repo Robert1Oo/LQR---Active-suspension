e765d25fb89198f9c5899f5011fcb4f71b904974
Roberto Nasrat <119348560+Robert1Oo@users.noreply.github.com>
2025-02-20 14:47:11 +0200

    Block diagram model

--- param.m
+++ param.m
@@ -1,22 +1,21 @@
-% M           = 30 
-% m           = 13
-% k           = 6921
-% kt          = 81000
-% c           = 0
-% ct          = 0
+clc
+clear all
+
+%% Parameters of the system used for the simulation
     
-    
-a   = 0.4           % eccentricity
-F   = 3             % frequency
-ms  = 34;           % Sprung Mass (kg)
-mus = 11;           % Unsprung Mass (kg)
-ks  = 6921;         % Suspension Stiffness (N/m)
-kus = 81000;        % Wheel stiffness (N/m)
-bs  = 0;            % Suspension Inherent Damping coefficient (sec/m)
-bus = 0;            % Wheel Inhenrent Damping coefficient (sec/m)
+ms  = 34;            % Sprung Mass (kg)
+mus = 11;             % Unsprung Mass (kg)
+ks  = 6921;          % Suspension Stiffness (N/m)
+kus = 81000;          % Wheel stiffness (N/m)
+bs  = 0;             % Suspension Inherent Damping coefficient (sec/m)
+bus = 0;              % Wheel Inhenrent Damping coefficient (sec/m)
+a   = 0.078;          % eccentricity
+F   = 0.3;            % frequency
+Sim_Time= 60;        % Simulation Initiation/ Time Setting
 
 
-%% System Dynamics for the Active Suspension system.
+
+%% System Dynamics for the system
 A = [ 0 1 0 -1 ;
     -ks/ms -bs/ms 0 bs/ms;
       0 0 0 1; 
@@ -37,20 +36,13 @@ D = [0 0;
      0 0;
      0 0;
      0 1/ms];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Controllability
 rank(ctrb(A,B))
 
 %% LQR Control law
-
-Q = diag([5, 4, 1, 1]);
-
-R = 0.1;
-
-K = lqr( A, B(:,2), Q, R ) 
-
-%% Simulation Initiation/ Time Setting
-Sim_Time= 10;
-
-sim('Suspension_model')
-
-
+   
+R = 1;                           % Control effort
+Q = diag([9990000, 999990, 9500, 0.1]);      % [zs - zus, zs_dot, zus - zr, zus_dot]
+K = lqr( A, B(:,2), Q, R )         % Compute LQR gain for the first input (actuator force)
+%%
